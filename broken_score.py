119c0dece902cd7162fc38879b50ea9c6e4d5e48
Julian Evan <julian.evan@my.jcu.edu.au>
2019-12-03 19:26:39 +0800

    change the format of the coding.

--- broken_score.py
+++ broken_score.py
@@ -5,13 +5,16 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 
 """
+def determine_result(score):
+    if score < 0 or score > 100:  # if score is below 0 or above 100
+        return ("Invalid Score")
+    elif score >= 90:  # if score is 90 or above
+        return ("Excellent")
+    elif score >= 50:  # if score is 50 or above
+        return ("Passable")
+    else:  # if score is below 50
+        return ("Bad")
+
 # TODO: Fix this!
 score = float(input("Enter score: "))
-if score < 0 or score > 100: #if score is below 0 or above 100
-    print("Invalid score")
-elif score >= 90:   #if score is 90 or above
-    print("Excellent")
-elif score >= 50:   #if score is 50 or above
-    print("Passable")
-else:              #if score is below 50
-    print("Bad")
+print(determine_result(score))
--- temperature.py
+++ temperature.py
@@ -7,7 +7,14 @@ Pseudocode for temperature conversion
 
 """
 
-
+def calculate_celsius():
+    celsius = float(input("Celsius: "))  # float so as to allow decimal input
+    fahrenheit = celsius * 9.0 / 5 + 32  # formula for celsius to fahrenheit conversion
+    print("Result: {:.2f} F".format(fahrenheit))  # displays fahrenheit, rounded to two decimal places.
+def calculate_fahrenheit():
+    fahrenheit = float(input("Fahrenheit: "))  # float so as to allow decimal input
+    celsius = (5 / 9) * (fahrenheit - 32)  # formula for fahrenheit to celsius conversion
+    print("Result: {:.2f} F".format(celsius))  # displays celsius, rounded to two decimal places
 
 MENU = """C - Convert Celsius to Fahrenheit
 
@@ -21,13 +28,9 @@ choice = input(">>> ").upper() #to make the inputted text in upper case
 
 while choice != "Q": #the program will loop until the user enters q
     if choice == "C": #if user enters c
-        celsius = float(input("Celsius: ")) #float so as to allow decimal input
-        fahrenheit = celsius * 9.0 / 5 + 32 #formula for celsius to fahrenheit conversion
-        print("Result: {:.2f} F".format(fahrenheit)) #displays fahrenheit, rounded to two decimal places.
+        calculate_celsius()
     elif choice == "F": #if choice is F
-        fahrenheit = float(input("Fahrenheit: "))#float so as to allow decimal input
-        celsius = (5/9)*(fahrenheit-32)#formula for fahrenheit to celsius conversion
-        print("Result: {:.2f} F".format(celsius)) #displays celsius, rounded to two decimal places
+        calculate_fahrenheit()
     else: #if choice is not C,F or Q.
         print("Invalid option")
         print(MENU)
